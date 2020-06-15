bffbec009f1b9b9d47c0a0a93fb9f8506882ccae
paulchabz <paulwchabz@gmail.com>
2020-06-15 01:15:58 +0100

    First intial commit of the repo for Tornament Tracker Application

---
+++ TournamentLibrary/TournamentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentLibrary
+{
+    public class TournamentModel
+    {
+        public string TornamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredNames { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+       
+    }
+}
